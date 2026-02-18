2460e5aab41168c5212dfce7fd88f32fa7edecc6
Tharun R <rajtharun08@gmail.com>
2026-02-18 11:13:44 +0530

    added loader (from json to mysql)

---
+++ loader.py
@@ -0,0 +1,38 @@
+import json
+import math
+from database import SessionLocal,engine
+from tables import Recipe 
+import tables
+
+tables.Base.metadata.create_all(bind=engine)
+
+
+def load():
+    with open('./US_recipes_null.json', 'r',encoding='utf-8') as file:
+        data = json.load(file)
+    db = SessionLocal()
+    try:
+        for item in data:
+            for key, value in item.items():
+                if isinstance(value, float) and math.isnan(value):
+                    item[key] = None
+            n_recipe= Recipe(
+                cuisine=item.get("cuisine"),
+                title=item.get("title"),
+                rating=item.get("rating"),
+                prep_time=item.get("prep_time"),
+                cook_time=item.get("cook_time"),
+                total_time=item.get("total_time"),
+                description=item.get("description"),
+                nutrients=item.get("nutrients"), 
+                serves=item.get("serves")
+            )
+            db.add(n_recipe)
+        db.commit()
+    except Exception as e:
+        db.rollback()
+    finally:
+        db.close()
+
+if __name__ == "__main__":
+    load()
